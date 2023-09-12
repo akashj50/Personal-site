8303511efeb7af838c59e2014863d0bd56600198
akashj50 <71134413+akashj50@users.noreply.github.com>
2023-09-12 01:24:27 -0400

    Update About1.js

--- src/Components/About1.js
+++ src/Components/About1.js
@@ -10,7 +10,7 @@ const About1 = () => {
             <img src={photo}></img>
             <p>Hi I'm Akash Jayathirtha,a Computer Engineering student at University of Waterloo.My main interests are math and programming as I wish to pursue a career in web development/software development.In my free time,I play sports such as Cricket,Swimming etc.</p>
             <br></br>
-            <p>Here is my resume</p>
+            <p>Here is my <a href={resume} download="Resume"><u>resume</u></a></p>
         </div>
 
       
